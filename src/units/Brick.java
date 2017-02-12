e1ab42ff551848f7a61ef33be1fbf1bca2757ce6
Krasimir Gyorev <krasi_job@mail.bg>
2017-02-12 21:38:12 +0200

    Remove Spirit class and move it methods into Brick class

--- src/units/Brick.java
+++ src/units/Brick.java
@@ -1,11 +1,10 @@
 package units;
 
-//TODO: Create the brick class
-
 import javax.swing.*;
+import java.awt.*;
 
-// The Brick class extends Sprite, and represents the breakable tiles.
-public class Brick extends Sprite {
+// The Brick class represents the breakable tiles.
+public class Brick {
 
 
     /* The destroyed attribute indicates whether the current brick is destroyed.
@@ -17,6 +16,11 @@ public class Brick extends Sprite {
      * no other functionality associated with changing this value
      * (like keeping values in limits, for example).
      */
+    protected Image image;
+    protected int width;
+    protected int height;
+    protected int x;
+    protected int y;
     public boolean destroyed;
     public int hitCount;
     public void hitBrick(){
@@ -35,10 +39,35 @@ public class Brick extends Sprite {
         }
     }
     public Brick(int x, int y) {
-        super(x, y);
+        this.x = x;
+        this.y = y;
+        width = height = 0;
+        image = null;
         hitCount =3;
         setImage(new ImageIcon(
                 this.getClass().getResource("/brick.png")).getImage());
         destroyed = false;
+    }
+    public void setImage(Image img) {
+        image = img;
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+    public Image getImage() { return image; }
+
+    public int getWidth() {return width; }
+    public int getHeight() { return height; }
+    public int getX() { return x; }
+
+    // Setter and getter methods for the Y attribute
+    public void setY(int y) { this.y = y; }
+    public int getY() { return y; }
+
+    /* Getter method for sprite rectangle. It is typically the rectangle of the
+     * image, but strictly speaking it is the bounding box of the sprite, used
+     * for collision detection.
+     */
+    public Rectangle getRect() {
+        return new Rectangle(x, y, image.getWidth(null), image.getHeight(null));
     }
 }
