1df9d466561867011c4f4cee5895eceead626357
tgenovska <tsvetelina_genovska@abv.bg>
2017-02-13 22:43:56 +0100

    Update Highscores.java

--- src/game/Highscores.java
+++ src/game/Highscores.java
@@ -9,13 +9,10 @@ import java.util.*;
 public class Highscores {
     public int score;
     public String playerName;
-    public long time; //still not clear how we will calculate time - Timer not ready;
 
-    public Highscores(int score, String playerName, long time) {
+    public Highscores(int score, String playerName) throws IOException {
         this.score = score;
         this.playerName = playerName;
-        this.time = time;
-
     }
 
     public int getScore() {
@@ -34,13 +31,6 @@ public class Highscores {
         this.playerName = playerName;
     }
 
-    public long getTime() {
-        return time;
-    }
-
-    public void setTime(long time) {
-        this.time = time;
-    }
 
     public void createTextFile() { //Text file with the top 10 scores (name, score, time) is created
         String outputPath = "/Highscores.txt";
@@ -56,13 +46,16 @@ public class Highscores {
         }
     }
 
-    //After the end of the game the score of the current user is compared to the top 10 scores
-    public void getScoresFromFile() {
+
+    public TreeMap<String, Integer> results;
+    public ArrayList<String> sortedResults;
+
+    public int returnMinResult() {
 
         int resultCurrentUser = score;
-        long timeCurrentUser = time;
         String nameCurrentUser = playerName;
         String inputPath = "/Highscores.txt";
+        int minResult = Integer.MAX_VALUE;
 
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(inputPath))) {
 
@@ -75,32 +68,24 @@ public class Highscores {
                 line = reader.readLine();
 
             }
-            LinkedHashMap<Integer, TreeMap<String, Long>> results = new LinkedHashMap<>();
+            results = new TreeMap<>();
 
-            int minResult = Integer.MAX_VALUE;
             for (String userResult : scoresFromFile) {
                 String[] userResultArgs = userResult.split(", ");
                 String[] userNameArgs = userResultArgs[0].split(":");
                 String userName = userNameArgs[1].trim();
                 String[] scoreArgs = userResultArgs[1].split(":");
                 int resultUser = Integer.parseInt(scoreArgs[1].trim());
-                String[] timeArgs = userResultArgs[2].split(":");
-                long timeUser = Long.parseLong(timeArgs[1].trim());
 
-                if (!results.containsKey(resultUser)) {
-                    results.put(resultUser, new TreeMap<>());
+                if (!results.containsKey(userName)) {
+                    results.put(userName, resultUser);
 
-                    if (!results.get(resultUser).containsKey(userName)) {
-                        results.get(resultUser).put(userName, timeUser);
-                    }
                     if (minResult > resultUser) {
                         minResult = resultUser;
                     }
                 } else {
-                    if (!results.get(resultUser).containsKey(userName)) {
-                        results.get(resultUser).put(userName, timeUser);
-                    } else {
-                        results.get(resultUser).put(userName, results.get(resultUser).get(userName) + timeUser);
+                    if (results.get(userName) < resultUser) {
+                        results.put(userName, resultUser);
                     }
                     if (minResult > resultUser) {
                         minResult = resultUser;
@@ -108,98 +93,78 @@ public class Highscores {
                 }
             }
 
-            if (score >= minResult || results.size() < 10) {
-                if (!results.containsKey(score)) {
-                    results.put(score, new TreeMap<>());
-                    results.get(score).put(playerName, time);
-                } else {
-                    if (!results.get(score).containsKey(playerName)) {
-                        results.get(score).put(playerName, time);
-                    } else {
-                        results.get(score).put(playerName, results.get(score).get(playerName) + time);
-                    }
-                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return minResult;
+    }
+    //applicable if player's score is higher than the minimal result
+    public void insertPlayer(String playerName, int score) {
+
+        if (!results.containsKey(playerName)) {
+            results.put(playerName, score);
+        } else {
+            if (results.get(playerName) < score) {
+                results.put(playerName, score);
             }
+        }
+    }
 
-            StringBuilder sb = new StringBuilder();
+    // the results are sorted after the new player is added
+    public ArrayList<String> sortScores() {
 
-            results.entrySet().stream()
-                    .sorted(Map.Entry.comparingByKey((v1, v2) -> v2.compareTo(v1)))
-                    .limit(10)
-                    .forEach(c -> {
-                        TreeMap<String, Long> orderedResults = c.getValue();
-                        orderedResults.entrySet().stream()
-                                .sorted(Map.Entry.comparingByValue((v1, v2) -> v1.compareTo(v2)))
-                                .forEach(pair -> {
-                                    sb.append("Name: " + pair.getKey() + ", Score: " + c.getKey() + ", Time: " + pair.getValue());
-                                    sb.append("\n");
+        sortedResults = new ArrayList<>();
+        results.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey((v1, v2) -> v2.compareTo(v1)))
+                .sorted((k1, k2) -> k1.getValue().compareTo(k2.getValue()))
+                .limit(10)
+                .forEach(pair -> {
 
-                                });
-                    });
+                    sortedResults.add("Name: " + pair.getKey() + ", Score: " + pair.getKey() + "\n");
 
+                });
+        return sortedResults;
 
-            String outputPath = "/temp.txt"; //The sorted map is saved in temp.txt to get numeration
+    }
+    public void saveFinalResults(){
+        String outputPathFinal = "/HighscoresFinal.txt";
 
-            try (PrintWriter writer = new PrintWriter(outputPath)) {
+        try (PrintWriter writerFinal = new PrintWriter(outputPathFinal)){
 
-                writer.print(sb.toString());
-
-            } catch (IOException e) {
-                e.printStackTrace();
+            for (String result : sortedResults) {
+                writerFinal.println(result);
             }
-            //The final scores get numeration and are saved in a fileFinal 
-            String inputPathTemp = "/temp.txt";
-            String outputPathFinal = "/HighscoresFinal.txt";
-
-            try (BufferedReader readerTemp = Files.newBufferedReader(Paths.get(inputPathTemp));
-                 PrintWriter writerFinal = new PrintWriter(outputPathFinal)){
-
-                String lineTemp = readerTemp.readLine();
-                int counter = 1;
-
-                while(line != null){
-                    writerFinal.println(counter + ". " + lineTemp);
-
-                    lineTemp = readerTemp.readLine();
-                    counter++;
-
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //The final sorted file replaces the original Highscores file
-            File f = new File("/HighScores.txt");
-            File g = new File("/HighscoresFinal.txt");
-            f.delete();
-            g.renameTo(f);
-
-            //Method for printing scores is created
-            printScores();
-
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void printScores(/*Graphics g*/) throws IOException {
-
-        int h = 100;
-        File fileToRead = new File("HighScores.txt");
-        LineNumberReader lnr = new LineNumberReader(new FileReader(fileToRead));
-        String line = lnr.readLine();
-        while (line != null) {
-            int rank = lnr.getLineNumber();
-            // g.drawString(rank + ". " + line, getWidth()/5, h);
-            h += 15;
-            line = lnr.readLine();
-        }
-        lnr.close();
+    public void makeNewScoreTable() throws IOException {
+        File f = new File("HighScores.txt");
+        File g = new File("temp.txt");
+        f.delete();
+        g.renameTo(f);
     }
 
 }
 
+
+
+
+   /* public void createTextFile() { //Text file with the top 10 scores (name, score) is created
+        String outputPath = "/Highscores.ser";
+        String testLine = "Name: Test, Score: 1";
+
+        try (FileOutputStream fos = new FileOutputStream(outputPath);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+
+            oos.writeObject(testLine);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
 
 
 
