f423804f156c167dcce3b290d7888c4788960d05
tgenovska <tsvetelina_genovska@abv.bg>
2017-03-06 15:12:40 +0100

    Update GameTimer.java

--- src/game/GameTimer.java
+++ src/game/GameTimer.java
@@ -6,38 +6,102 @@ import java.util.TimerTask;
 
 public class GameTimer {
 
+    private static long startTime;
+    private static long endTime;
+    private static long elapsedTime;
 
-    private Timer timer;
-    private int seconds;
+
 
     public GameTimer() {
 
-        timer = new Timer();
+      //this.setStartTime(startTime);
+    }
+    
+    //When the GameTimer is started it gets System.currentTimeMillis() to get the startTime
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+    
+    //Calculates elapsed time
+    public long SetElapsedTime(){
+        long elapsedTime = (System.currentTimeMillis() - getStartTime());
+        long elapsedSeconds = elapsedTime / 1000;
+
+        long finalMinutes = elapsedSeconds/60;
+        long finalSeconds = elapsedSeconds%60;
+       /* System.out.println(finalSeconds);
+        System.out.println(finalMinutes);*/
+        return elapsedSeconds;
+    }
+    public long GetElapsedTime(){
+        return this.elapsedTime;
     }
 
 
-    public void initializeTimer() {
+
+    public long getFinalMinutes(){
+        long finalMinutes = GetElapsedTime()/60;
+        return finalMinutes;
+    }
+    public long getFinalSeconds(){
+         long finalSeconds = GetElapsedTime()%60;
+        return finalSeconds;
+    }
+
+    public void setEndTime(long endTime){
+        long endTimeLevel = endTime;
+
+    }
+    public long getStartTime(){
+        return this.startTime;
+    }
+
+    public long getEndTime(){
+        return this.endTime;
+    }
+
+
+
+
+    /*public void initializeTimer() {
+        thread = new Thread();
+        this.seconds = 60;
+
+        for (int i = 60; i >= 1; i--) {
+            try {
+                thread.sleep(1000);
+                seconds = i;
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
+            }
+
+
+        }
+}*/
+    /*public void initializeTimer() {
 
         this.seconds = 60;
-        TimerTask task = new TimerTask() {
 
-            @Override
-            public void run() {
-                if (seconds == 0) {
-                    timer.cancel();
-                }
-                seconds--;
-            }
-        };
+        this.timer.schedule(
+                new TimerTask() {
 
-        this.start(task);
-    }
+                    @Override
+                    public void run() {
+                        if (seconds == 0) {
+                            return;
+                        }
+                        seconds--;
+                    }
+                }, 1000, 1000);
 
-    private void start(TimerTask task) {
-        this.timer.scheduleAtFixedRate(task, 1000, 1000);
+        //this.start(task);
+    }*/
+
+      /*private void start(TimerTask task) {
+         this.timer.scheduleAtFixedRate(task, 1000, 1000);
     }
 
     public int getSeconds() {
         return this.seconds;
-    }
+    }*/
 }
